c95e429d1586a973b12fd0d42230ae48d63085dd
VitorOM2 <viktormarque2053@gmail.com>
2021-09-27 00:54:40 -0300

    Projeto Adiconado

---
+++ nome_igual_main/modulo1.py
@@ -0,0 +1,15 @@
+# ***********************************
+# if __name__ == '__main__'
+# ***********************************
+
+# Para que usar?
+# 1. O módulo pode ser executado sozinho
+#    o
+# 2. Modulo pode ser importado e ser usado em outros módulos
+
+def oi():
+    print("Olá!")
+
+
+if __name__ == '__main__':
+    oi()
