fea480747af86324640e63847296762368b1c4ab
VitorOM2 <viktormarque2053@gmail.com>
2021-10-05 11:22:45 -0300

    Criada uma listbox

--- tela_listbox.py
+++ tela_listbox.py
@@ -5,10 +5,30 @@ from tkinter import *
 
 # ==================== Instanciação ====================
 tela = Tk()
+
+# ===== List Box =====
+listbox = Listbox(tela,
+    bg    = '#f7ffde',
+    font  = ('Constantia', 35),
+    width = 12,
+    
+    )
+
+listbox.insert(1, 'Pizza')
+listbox.insert(2, 'Macarronada')
+listbox.insert(3, 'Pão de alho')
+listbox.insert(4, 'sopa')
+listbox.insert(5, 'Salada')
+
+listbox.config( height = listbox.size() )
+
 # ==================== Instanciação ====================
 
 
 # ==================== Mostrar ===================
+
+# ===== List Box =====
+listbox.pack()
 
 # ===== tela =====
 tela.mainloop()
