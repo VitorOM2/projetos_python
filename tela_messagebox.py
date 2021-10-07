17a36adc62abd6f0679b5c17e9b4f8281e364242
VitorOM2 <viktormarque2053@gmail.com>
2021-10-06 23:21:17 -0300

    Criado mais exemplos de messagebox

--- tela_messagebox.py
+++ tela_messagebox.py
@@ -15,11 +15,24 @@ def clique():
     #messagebox.showerror(title = 'Erro!',
     #    message = 'Olá')
 
-    if messagebox.askokcancel(title = 'Sim ou não', message = 'Sim ou não'):
+    if messagebox.askokcancel(title = 'Ok ou cancelar', message = 'Ok ou cancelar'):
+        print ('ok')
+    else:
+        print ('cancelar')
+
+    if messagebox.askretrycancel(title = 'Tentar de novo?', message = 'Tentar de novo'):
+        print ('Você tentou de novo')
+    else:
+        print ('Você não tentou de novo')
+
+    if messagebox.askyesno(title = 'Sim ou não', message = "Sim ou não"):
         print ('Sim')
     else:
         print ('Não')
 
+    print (messagebox.askquestion(title = 'Pergunta', message = 'Gosta de pizza?') )
+
+    print (messagebox.askyesnocancel(title = 'Sim, Não Cancelar', message = 'Sim, não ou cancelar') )
 # ==================== Funções ====================
 
 
