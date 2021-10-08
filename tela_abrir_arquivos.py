3a2805d5412339b780ab9e55d4516629aa099ca6
VitorOM2 <viktormarque2053@gmail.com>
2021-10-08 20:39:34 -0300

    Criado botão para abrir arquivos

--- tela_abrir_arquivos.py
+++ tela_abrir_arquivos.py
@@ -1,13 +1,33 @@
 # ==================== Importações ====================
 from tkinter import *
+from tkinter import filedialog  
 # ==================== Importações ====================
 
 
+# ==================== Funções ====================
+# ==================== Funções ====================
+
+
+
 # ==================== Instanciação ====================
+
+# ===== Tela =====
 tela = Tk()
+
+# ===== Botões =====
+btn_abrir_arquivos = Button(
+    text    = 'Abrir',
+    command = abrir_arquivos)
+
 # ==================== Instanciação ====================
 
 
 # ==================== Mostrar ====================
+
+# ===== Botões =====
+btn_abrir_arquivos.pack
+
+# ===== Tela =====
 tela.mainloop()
+
 # ==================== Mostrar ====================
