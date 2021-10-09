9dfc40cdc1d78d2371345d7ee74fa6f26e8cec56
VitorOM2 <viktormarque2053@gmail.com>
2021-10-08 23:14:03 -0300

    Criado uma barra de menu na página

--- tela_barra_menu.py
+++ tela_barra_menu.py
@@ -8,7 +8,28 @@ from tkinter import *
 # ===== Tela =====
 tela = Tk()
 
+# ===== Menu =====
+menu_barra   = Menu(tela)
+menu_arquivo = Menu(menu_barra,
+    tearoff = 0)
+
 # ==================== Instanciação ====================
+
+
+# ==================== Configurações ====================
+
+# ===== Tela =====
+tela.config(menu = menu_barra)
+
+# ===== Menu =====
+menu_barra.add_cascade(label = 'Arquivo', menu = menu_arquivo)
+
+menu_arquivo.add_command(label = 'Abrir')
+menu_arquivo.add_command(label = 'Salvar')
+menu_arquivo.add_separator()
+menu_arquivo.add_command(label = 'Sair')
+
+# ==================== Configurações ====================
 
 
 # ==================== Mostrar ====================
