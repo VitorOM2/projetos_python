7eea03bcd7beb9d8eec6e3b1670bf56b7806c5f0
VitorOM2 <viktormarque2053@gmail.com>
2021-10-08 21:02:07 -0300

    Adicionado: um botão de salvar arquivo e uma caixa de texto

--- tela_salvar_arquivos.py
+++ tela_salvar_arquivos.py
@@ -4,15 +4,35 @@ from tkinter import filedialog
 # ==================== Importações ====================
 
 
+# ==================== Funções ====================
+def salvar_arquivo():
+    pass
+# ==================== Funções ====================
+
+
 # ==================== Instanciação ====================
 
 # ===== Tela =====
 tela = Tk()
 
+# ===== Botões =====
+btn_salvar = Button(
+    text    = 'Salvar',
+    command = salvar_arquivo)
+
+# ===== Caixa de texto =====
+caixa_texto = Text()
+
 # ==================== Instanciação ====================
 
 
 # ==================== Mostrar ====================
+
+# ===== Botões =====
+btn_salvar.pack()
+
+# ===== Caixa de texto =====
+caixa_texto.pack()
 
 # ===== Tela =====
 tela.mainloop()
