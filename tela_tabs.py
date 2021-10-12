aeaa8f3246b611d55b3f37e1d403a03a36568043
VitorOM2 <viktormarque2053@gmail.com>
2021-10-11 23:48:07 -0300

    Criado um notebook e duas tabs

--- tela_tabs.py
+++ tela_tabs.py
@@ -1,6 +1,6 @@
 # ==================== Importações ====================
 from tkinter import *
-from tkinter import tkk
+from tkinter import ttk
 # ==================== Importações ====================
 
 
@@ -9,14 +9,30 @@ from tkinter import tkk
 # ===== Tela =====
 tela = Tk()
 
+# ===== Notebook =====
+notebook = ttk.Notebook(tela) # Dispositivo que gerência uma coleção de janelas/displays
+
+tab1 = Frame(notebook) # Novo frame para a tab1
+tab2 = Frame(notebook) # Novo frame para a tab2
+
 # ==================== Instanciação ====================
 
 
 # ==================== Configurações ====================
+
+# ===== Notebook =====
+notebook.add(tab1, text = 'Tab 1')
+notebook.add(tab2, text = 'Tab 2')
+
 # ==================== Configurações ====================
 
 
 # ==================== Mostrar ====================
+
+# ===== Notebook =====
+notebook.pack(
+    expand = TRUE, # Expande para preencher qualquer espaço não utilizad
+    fill = 'both') # Preenche espaço nos eixos x e y
 
 # ===== Tela =====
 tela.mainloop()
