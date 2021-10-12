33e9d303750e904158360c1c27d4cfd8d3982b9c
VitorOM2 <viktormarque2053@gmail.com>
2021-10-11 23:52:09 -0300

    Criado duas labels

--- tela_tabs.py
+++ tela_tabs.py
@@ -15,6 +15,17 @@ notebook = ttk.Notebook(tela) # Dispositivo que gerência uma coleção de janel
 tab1 = Frame(notebook) # Novo frame para a tab1
 tab2 = Frame(notebook) # Novo frame para a tab2
 
+# ===== Rótulos =====
+rotulo_tab1 = Label(tab1,
+    text   = 'Tab1',
+    width  = 50,
+    height = 25)
+
+rotulo_tab2 = Label(tab2,
+    text   = 'Tab2',
+    width  = 50,
+    height = 25)
+
 # ==================== Instanciação ====================
 
 
@@ -33,6 +44,10 @@ notebook.add(tab2, text = 'Tab 2')
 notebook.pack(
     expand = TRUE, # Expande para preencher qualquer espaço não utilizad
     fill = 'both') # Preenche espaço nos eixos x e y
+
+# ===== Rótulos =====
+rotulo_tab1.pack()
+rotulo_tab2.pack()
 
 # ===== Tela =====
 tela.mainloop()
