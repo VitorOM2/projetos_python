efc7518e3c96b7abcef56715cd9257555b3b564e
VitorOM2 <viktormarque2053@gmail.com>
2021-10-12 22:49:22 -0300

    Criado um label para nome

--- teala_grid.py
+++ teala_grid.py
@@ -8,10 +8,19 @@ from tkinter import *
 # ===== Tela =====
 tela = Tk()
 
+# ===== Grid ===== Grid() = gerenciador geométrico que organiza widgets em uma forma de tabela
+
+# ===== Labels =====
+rotulo_nome = Label (tela,
+    text = 'Primeiro nome: ',)
+
 # ==================== Instanciação ====================
 
 
 # ==================== Mostrar ====================
+
+# ===== Labels =====
+rotulo_nome.pack()
 
 # ===== Tela =====
 tela.mainloop()
