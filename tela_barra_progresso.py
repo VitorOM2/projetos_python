37e4868359c9b8f6f5e7a4e0e0addef0a5e26ea1
VitorOM2 <viktormarque2053@gmail.com>
2021-10-12 23:21:21 -0300

    Criado um botão e uma função ativada pelo botão

--- tela_barra_progresso.py
+++ tela_barra_progresso.py
@@ -4,14 +4,25 @@ from tkinter.ttk import *
 import time
 # ==================== Importações ====================
 
+# ==================== Funções ====================
+def baixar():
+    pass
+# ==================== Funções ====================
+
 # ==================== Instanciação ====================
 
 # ===== Tela =====
 tela = Tk()
 
+# ===== Botões =====
+btn_download = Button (tela, text = 'Download', command = baixar)
+
 # ==================== Instanciação ====================
 
 # ==================== Mostrar ====================
+
+# ===== Botões =====
+btn_download.pack()
 
 # ===== Tela =====
 tela.mainloop()
