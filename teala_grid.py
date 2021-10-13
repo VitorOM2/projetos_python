6f89c68f04f68eb172ddf648d029fb8135fcc1ad
VitorOM2 <viktormarque2053@gmail.com>
2021-10-12 23:01:48 -0300

    Adicionado mais labels e caixas de entradas

--- teala_grid.py
+++ teala_grid.py
@@ -11,11 +11,19 @@ tela = Tk()
 # Grid() = gerenciador geométrico que organiza widgets em uma forma de tabela
 
 # ===== Labels =====
-rotulo_nome = Label (tela,
-    text = 'Primeiro nome: ',)
+rotulo_nome      = Label (tela,
+    text = 'Primeiro nome: ')
+
+rotulo_sobrenome = Label (tela,
+    text = 'Sobrenome: ')
+
+rotulo_email = Label (tela,
+    text = 'Email: ')
 
 # ===== Caixas de entradas =====
-caixa_nome = Entry(tela)
+caixa_nome      = Entry(tela)
+caixa_sobrenome = Entry(tela)
+caixa_email     = Entry(tela)
 
 # ==================== Instanciação ====================
 
@@ -23,10 +31,14 @@ caixa_nome = Entry(tela)
 # ==================== Mostrar ====================
 
 # ===== Labels =====
-rotulo_nome.grid(row = 0, column = 0)
+rotulo_nome.grid      (row = 0, column = 0)
+rotulo_sobrenome.grid (row = 1, column = 0)
+rotulo_email.grid     (row = 2, column = 0)
 
 # ===== Caixa de entrada =====
-caixa_nome.grid(row = 0, column = 1)
+caixa_nome.grid      (row = 0, column = 1)
+caixa_sobrenome.grid (row = 1, column = 1)
+caixa_email.grid     (row = 2, column = 1)
 
 # ===== Tela =====
 tela.mainloop()
