66cba8fed11b603670012be42a4cf778fb08845e
VitorOM2 <viktormarque2053@gmail.com>
2021-10-12 23:45:32 -0300

    Cirado uma label para mostrar a porcentagem da barra de progresso

--- tela_barra_progresso.py
+++ tela_barra_progresso.py
@@ -4,6 +4,7 @@ from tkinter.ttk import *
 import time
 # ==================== Importações ====================
 
+
 # ==================== Funções ====================
 def baixar():
     task = 10
@@ -13,14 +14,23 @@ def baixar():
         time.sleep(1)
         barra ['value'] += 10
         x += 1
+        porcentagem.set ( str (int ( ( x / task ) * 100)  ) + '%' )
         tela.update_idletasks() # Atualiza a tela a cada alteração
 
 # ==================== Funções ====================
 
-# ==================== Instanciação ====================
 
-# ===== Tela =====
+# ==================== Instanciação da Tela ====================
 tela = Tk()
+# ==================== Instanciação da Tela ====================
+
+
+# ==================== Variáveis Globais ====================
+porcentagem = StringVar()
+# ==================== Variáveis Globais ====================
+
+
+# ==================== Instanciação dos widgets ====================
 
 # ===== Botões =====
 btn_download = Button (tela, text = 'Download', command = baixar)
@@ -28,12 +38,19 @@ btn_download = Button (tela, text = 'Download', command = baixar)
 # ===== Barra de progresso =====
 barra = Progressbar (tela, orient = HORIZONTAL, length = 200)
 
+# ===== Labels =====
+rotulo_porcentagem = Label (tela, textvariable = porcentagem)
+
 # ==================== Instanciação ====================
+
 
 # ==================== Mostrar ====================
 
 # ===== Barra de progresso =====
 barra.pack( pady = 10 )
+
+# ===== Labels =====
+rotulo_porcentagem.pack()
 
 # ===== Botões =====
 btn_download.pack()
