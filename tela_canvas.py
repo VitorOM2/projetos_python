8ab7b46633c3a15918d75a93091ee8534043f358
VitorOM2 <viktormarque2053@gmail.com>
2021-10-14 22:16:55 -0300

    Criado um canvas

--- tela_canvas.py
+++ tela_canvas.py
@@ -4,5 +4,15 @@ from tkinter import *
 # ==================== Instanciação da Tela ====================
 tela = Tk()
 
+# ==================== Instanciação dos widgets ====================
+
+# ===== Canvas =====
+canvas = Canvas ( tela, height = 500, width = 500 )
+
+# ==================== Mostrar ====================
+
+# ===== Canvas =====
+canvas.pack()
+
 # ===== Tela =====
 tela.mainloop()
