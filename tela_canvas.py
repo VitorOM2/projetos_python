6874772aadf50438b8000504382a181261797892
VitorOM2 <viktormarque2053@gmail.com>
2021-10-14 22:22:51 -0300

    Criado duas linhas no canvas

--- tela_canvas.py
+++ tela_canvas.py
@@ -9,6 +9,9 @@ tela = Tk()
 # ===== Canvas =====
 canvas = Canvas ( tela, height = 500, width = 500 )
 
+canvas.create_line ( 0, 0, 500, 500, fill = 'blue', width = 5 ) # Cria um linha (Inicio:x,y, Final:x,y)
+canvas.create_line ( 0, 500, 500, 0, fill = 'red' , width = 5 )
+
 # ==================== Mostrar ====================
 
 # ===== Canvas =====
