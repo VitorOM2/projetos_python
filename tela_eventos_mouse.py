48b0d3fb5a504dfade47b0c6278a98dff06bd6e3
VitorOM2 <viktormarque2053@gmail.com>
2021-10-15 23:42:41 -0300

    Função atualizada "clique_mouse" atualizada para quando houver clique com o mouse

--- tela_eventos_mouse.py
+++ tela_eventos_mouse.py
@@ -3,12 +3,12 @@ from tkinter import *
 
 
 # ==================== Funções ====================
-def clique_mouse():
-    pass
+def clique_mouse(evento):
+    print ( 'Você apertou o botão do mouse' )
 
 # ==================== Instanciação da Tela ====================
 tela = Tk()
-
+tela.bind( '<Button-1>' , clique_mouse )
 # ==================== Mostrar ====================
 
 # ===== Tela =====
