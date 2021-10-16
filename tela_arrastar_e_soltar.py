3395c031fcdef9eb5dd30bbf0fd6a2be6535c7eb
VitorOM2 <viktormarque2053@gmail.com>
2021-10-15 23:58:11 -0300

    Criado um rótulo

--- tela_arrastar_e_soltar.py
+++ tela_arrastar_e_soltar.py
@@ -6,7 +6,16 @@ from tkinter import *
 tela = Tk()
 
 
+# ==================== Instanciação dos widgets ====================
+
+# ===== Labels =====
+rotulo1 = Label (tela, bg = 'blue', height = 5, width = 10)
+
+
 # ==================== Mostrar ====================
+
+# ===== Labels =====
+rotulo1.place ( x = 0, y = 0)
 
 # ===== Tela =====
 tela.mainloop()
