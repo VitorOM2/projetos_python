29a8f48e36db8d35a39342f3f10ba8c113d882b8
VitorOM2 <viktormarque2053@gmail.com>
2021-10-15 23:48:49 -0300

    Função atualizada para mostrar as coordenadas de onde foi clicado

--- tela_eventos_mouse.py
+++ tela_eventos_mouse.py
@@ -4,11 +4,14 @@ from tkinter import *
 
 # ==================== Funções ====================
 def clique_mouse(evento):
-    print ( 'Você apertou o botão do mouse' )
+    print ( 'Coordenadas do clique: X: ' + str ( evento.x ) + ", Y: " + str(evento.y) )
 
 # ==================== Instanciação da Tela ====================
 tela = Tk()
-tela.bind( '<Button-1>' , clique_mouse )
+tela.bind( '<Button-1>' , clique_mouse ) # Botão esquerdo
+tela.bind( '<Button-2>' , clique_mouse ) # Scroll
+tela.bind( '<Button-3>' , clique_mouse ) # Botão direito
+
 # ==================== Mostrar ====================
 
 # ===== Tela =====
