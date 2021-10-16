bc738aeb28c9609467cb61e00ad43755ea6b579f
VitorOM2 <viktormarque2053@gmail.com>
2021-10-16 00:05:58 -0300

    Criado uma função "arrastar_inicio"

--- tela_arrastar_e_soltar.py
+++ tela_arrastar_e_soltar.py
@@ -1,15 +1,32 @@
 # ==================== Importações ====================
 from tkinter import *
+# ==================== Importações ====================
+
+
+# ==================== Funções ====================
+def arrastar_inicio(evento):
+    rotulo1.inicio_X = evento.x
+    rotulo1.inicio_Y = evento.y
+# ==================== Funções ====================
 
 
 # ==================== Instanciação da Tela ====================
 tela = Tk()
+# ==================== Instanciação da Tela ====================
 
 
 # ==================== Instanciação dos widgets ====================
 
 # ===== Labels =====
 rotulo1 = Label (tela, bg = 'blue', height = 5, width = 10)
+# ==================== Instanciação dos widgets ====================
+
+
+# ==================== Configurações ====================
+
+# ===== Labels =====
+rotulo1.bind ( ' <Button-1> ', arrastar_inicio )
+# ==================== Configurações ====================
 
 
 # ==================== Mostrar ====================
@@ -19,3 +36,4 @@ rotulo1.place ( x = 0, y = 0)
 
 # ===== Tela =====
 tela.mainloop()
+# ==================== Mostrar ====================
