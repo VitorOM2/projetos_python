bd999147007913785cc102cfeda9b96eda128b91
VitorOM2 <viktormarque2053@gmail.com>
2021-10-18 22:57:59 -0300

    Adicionado um loop para rodar a animação

--- tela_animacoes.py
+++ tela_animacoes.py
@@ -19,6 +19,7 @@ tela = Tk()
 
 # ===== Canvas =====
 canvas = Canvas (tela, width = DIAMETRO, height = TAMANHO)
+canvas.pack()
 
 # ===== Imagem =====
 foto_imagem        = PhotoImage ( file = 'alien.png' )
@@ -27,10 +28,17 @@ foto_imagem_canvas = canvas.create_image (0, 0, image = foto_imagem, anchor = NW
 # ==================== Instanciação dos widgets ====================
 
 
+# ==================== Loop ====================
+while True:          # Loop para atualizar a tela durante animação
+    coordenadas = canvas.coords ( foto_imagem_canvas )      # Pega as coordenadas da imagem
+    print (coordenadas)
+    tela.update ()                                          # Atualiza a tela
+    time.sleep  (0.01)                                      # Pausa o loop por determinado tempo 
+# ==================== Loop ====================
+
+
 # ==================== Mostrar ====================
 
-# ===== Canvas =====
-canvas.pack()
 
 # ===== Tela =====
 tela.mainloop()
