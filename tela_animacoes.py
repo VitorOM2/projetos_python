b0e875cdfec73287216ec1a24beb0c6d2c1bd6d4
VitorOM2 <viktormarque2053@gmail.com>
2021-10-18 23:03:42 -0300

    corrigido proble com canvas e adicionado movimento para a imagem

--- tela_animacoes.py
+++ tela_animacoes.py
@@ -4,15 +4,18 @@ import time
 # ==================== Importações ====================
 
 
-# ==================== Constantes e Variáveis ====================
-DIAMETRO = 500
-TAMANHO  = 500
-# ==================== Constantes e Variáveis ====================
-
-
 # ==================== Instanciação da Tela ====================
 tela = Tk()
 # ==================== Instanciação da Tela ====================
+
+
+# ==================== Constantes e Variáveis ====================
+DIAMETRO = 500
+TAMANHO  = 500
+
+velocidade_x = 1 # Variável para controlar a velocidade que a animação vai se mover no eixo X
+velocidade_y = 1 # Variável para controlar a velocidade que a animação vai se mover no eixo Y
+# ==================== Constantes e Variáveis ====================
 
 
 # ==================== Instanciação dos widgets ====================
@@ -32,13 +35,13 @@ foto_imagem_canvas = canvas.create_image (0, 0, image = foto_imagem, anchor = NW
 while True:          # Loop para atualizar a tela durante animação
     coordenadas = canvas.coords ( foto_imagem_canvas )      # Pega as coordenadas da imagem
     print (coordenadas)
+    canvas.move (foto_imagem_canvas, velocidade_x, 0)
     tela.update ()                                          # Atualiza a tela
     time.sleep  (0.01)                                      # Pausa o loop por determinado tempo 
 # ==================== Loop ====================
 
 
 # ==================== Mostrar ====================
-
 
 # ===== Tela =====
 tela.mainloop()
