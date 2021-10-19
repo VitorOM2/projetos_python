96978e8ca7b2237196d693b216d06b7e6d5d793c
VitorOM2 <viktormarque2053@gmail.com>
2021-10-18 22:32:19 -0300

    Adicionado um canvas e duas constantes

--- tela_animacoes.py
+++ tela_animacoes.py
@@ -4,6 +4,12 @@ import time
 # ==================== Importações ====================
 
 
+# ==================== Constantes e Variáveis ====================
+DIAMETRO = 500
+TAMANHO  = 500
+# ==================== Constantes e Variáveis ====================
+
+
 # ==================== Instanciação da Tela ====================
 tela = Tk()
 # ==================== Instanciação da Tela ====================
@@ -12,11 +18,15 @@ tela = Tk()
 # ==================== Instanciação dos widgets ====================
 
 # ===== Canvas =====
+canvas = Canvas (tela, width = DIAMETRO, height = TAMANHO)
 
 # ==================== Instanciação dos widgets ====================
 
 
 # ==================== Mostrar ====================
+
+# ===== Canvas =====
+canvas.pack()
 
 # ===== Tela =====
 tela.mainloop()
