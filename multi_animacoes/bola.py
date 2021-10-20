66873f41d1f9e0c0ddccbd1f500dbc9c09b64259
VitorOM2 <viktormarque2053@gmail.com>
2021-10-19 22:23:40 -0300

    Criado objeto

---
+++ multi_animacoes/bola.py
@@ -0,0 +1,8 @@
+class MyClass:
+
+  def __init__ (self, canvas, x, y, diametro, velocidade_x, velocidade_y, cor):
+    self.canvas = canvas
+    self.image  = canvas.create_oval(x, y, diametro, diametro, fill = cor)  # Primeito diametro serve para o diametro
+                                                                # O segundo serve para a altura
+    self.velocidade_x = velocidade_x
+    self.velocidade_y = velocidade_y
