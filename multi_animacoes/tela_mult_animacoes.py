f93433f0ff6f507bee3f19c4f87a2f898751525a
VitorOM2 <viktormarque2053@gmail.com>
2021-10-19 23:07:46 -0300

    Criado while loop para gerar a animação

--- multi_animacoes/tela_mult_animacoes.py
+++ multi_animacoes/tela_mult_animacoes.py
@@ -8,18 +8,25 @@ tela = Tk()
 
 
 # ==================== Constantes e Variáveis ====================
-DIAMETRO = 500
-ALTURA   = 500
+LARGURA = 500
+ALTURA  = 500
 
 
 # ==================== Instanciação dos widgets e objetos ====================
 
 # ===== Canvas =====
-canvas = Canvas (tela, width = DIAMETRO, height = ALTURA)
+canvas = Canvas (tela, width = LARGURA, height = ALTURA)
 canvas.pack()
 
 # ===== Bolas (Objetos) =====
 bola_volei = Bola(canvas, 0, 0, 100, 1, 1, 'white')
+
+
+# ==================== Loop ====================
+while True: # Loop que cria a animação e atualiza a tela
+    bola_volei.mover()
+    tela.update()
+    time.sleep(0.01)
 
 
 # ==================== Criar Tela ====================
