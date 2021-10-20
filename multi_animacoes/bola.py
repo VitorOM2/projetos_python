e26b7d45a27cc4b1a5c940055368584bcc709075
VitorOM2 <viktormarque2053@gmail.com>
2021-10-19 22:35:53 -0300

    Criado a função: Mover

--- multi_animacoes/bola.py
+++ multi_animacoes/bola.py
@@ -1,8 +1,14 @@
 class Bola:
 
-  def __init__ (self, canvas, x, y, diametro, velocidade_x, velocidade_y, cor):
-    self.canvas = canvas
-    self.image  = canvas.create_oval(x, y, diametro, diametro, fill = cor)  # Primeito diametro serve para o diametro
-                                                                # O segundo serve para a altura
-    self.velocidade_x = velocidade_x
-    self.velocidade_y = velocidade_y
+    # ==================== Construtor ====================
+    def __init__ (self, canvas, x, y, diametro, velocidade_x, velocidade_y, cor):
+        self.canvas = canvas
+        self.imagem  = canvas.create_oval(x, y, diametro, diametro, fill = cor)  # Primeito diametro serve para o diametro
+                                                                    # O segundo serve para a altura
+        self.velocidade_x = velocidade_x
+        self.velocidade_y = velocidade_y
+
+    # ==================== Funções ====================
+    def mover (self):
+        coordenadas = self.canvas.coords ( self.imagem )
+        print (coordenadas)
