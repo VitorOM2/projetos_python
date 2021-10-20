413ce71e82af72a3bd95953616268c28ea89dc3e
VitorOM2 <viktormarque2053@gmail.com>
2021-10-19 22:26:06 -0300

    Criado um canvas

--- multi_animacoes/tela_mult_animacoes.py
+++ multi_animacoes/tela_mult_animacoes.py
@@ -5,5 +5,18 @@ import time
 # ==================== Instanciação da Tela ====================
 tela = Tk()
 
+
+# ==================== Constantes e Variáveis ====================
+DIAMETRO = 500
+ALTURA   = 500
+
+
+# ==================== Instanciação dos widgets e objetos ====================
+
+# ===== Canvas =====
+canvas = Canvas (tela, width = DIAMETRO, height = ALTURA)
+canvas.pack()
+
+
 # ==================== Criar Tela ====================
 tela.mainloop()
