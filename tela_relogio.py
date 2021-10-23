1e396da64b6f8276e16c8d70d01eb841411b1a40
VitorOM2 <viktormarque2053@gmail.com>
2021-10-22 23:28:47 -0300

    Label para o dia da semana adicionado

--- tela_relogio.py
+++ tela_relogio.py
@@ -11,6 +11,10 @@ tela = Tk()
 def update():
     string_tempo = strftime ( '%I:%M:%S %p' )
     rotulo_tempo.config ( text = string_tempo )
+
+    string_dia = strftime ( '%A' )
+    rotulo_dia.config ( text = string_dia )
+
     rotulo_tempo.after (1000, update) # Atualiza a tela com o horário
 
 # ==================== Instanciação dos widgets e objetos ====================
@@ -19,6 +23,8 @@ def update():
 rotulo_tempo = Label (tela, font = ( 'arial', 25 ), fg = '#00FFFF', bg = "black" )
 rotulo_tempo.pack()
 
+rotulo_dia = Label (tela, font = ( 'arial', 25 ) )
+rotulo_dia.pack()
 
 update() # Chama uma função para mostrar o horário
 
