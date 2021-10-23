4e28c7a1c17916d12ddfa30291d23a930d882ccd
VitorOM2 <viktormarque2053@gmail.com>
2021-10-22 23:17:34 -0300

    Criada função para mostrar a hora e atualizar a tela

--- tela_relogio.py
+++ tela_relogio.py
@@ -7,11 +7,20 @@ from time    import *
 tela = Tk()
 
 
+# ==================== Funções ====================
+def update():
+    string_tempo = strftime ( '%I:%M:%S %p' )
+    rotulo_tempo.config ( text = string_tempo )
+    rotulo_tempo.after (1000, update) # Atualiza a tela com o horário
+
 # ==================== Instanciação dos widgets e objetos ====================
 
 # ===== Labels =====
-rotulo_tempo = Label (tela, font = ('arial', 25), fg = '#00FFFF', bg = "black" )
-rotulo_tempo.pack
+rotulo_tempo = Label (tela, font = ( 'arial', 25 ), fg = '#00FFFF', bg = "black" )
+rotulo_tempo.pack()
+
+
+update() # Chama uma função para mostrar o horário
 
 
 # ==================== Criar Tela ====================
