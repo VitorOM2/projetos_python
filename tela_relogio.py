0f4833d63670d5c33ea69c4dfa3dcbe7ce9fd58c
VitorOM2 <viktormarque2053@gmail.com>
2021-10-22 23:00:08 -0300

    Tela criada

---
+++ tela_relogio.py
@@ -0,0 +1,11 @@
+# ==================== Importações ====================
+from tkinter import *
+from time    import *
+
+
+# ==================== Instanciação da Tela ====================
+tela = Tk()
+
+
+# ==================== Criar Tela ====================
+tela.mainloop()
