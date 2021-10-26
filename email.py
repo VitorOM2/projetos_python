516e2851a4a2eca57ef6f29a39ef51f8c35f6ebd
VitorOM2 <viktormarque2053@gmail.com>
2021-10-25 23:28:01 -0300

    Projeto criado

---
+++ email.py
@@ -0,0 +1,10 @@
+# ==================== Importações ====================
+import smtplib
+
+
+# ==================== Variáveis ====================
+remetente    = "viktormarque2053@gmail.com"
+destinatario = "viktormarque2053@gmail.com"
+senha        = ""
+assunto      = "Python - teste de E-mail"
+corpo        = "Eu escrevi um email"
