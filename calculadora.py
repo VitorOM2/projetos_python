a0f55f812d9407fa586f70518f8257b317bbae19
VitorOM2 <viktormarque2053@gmail.com>
2021-10-27 23:30:11 -0300

    Criado um label para exibição de equações e resultados

--- calculadora.py
+++ calculadora.py
@@ -8,5 +8,17 @@ tela.geometry ( "500x500"   ) # Tamanho da tela
 tela.title    ("Calculadora") # Titulo da tela
 
 
+# ==================== Constantes e Variáveis ====================
+texto_equacao  = ""
+rotulo_equacao = StringVar()
+
+
+# ==================== Instanciação dos widgets ====================
+# ===== Labels =====
+rotulo_exibicao = Label( # Rotulo para exibir as equações e resultados
+    tela, textvariable = rotulo_equacao, font = ('consolas', 20), bg = "white", width = 20, height = 2 )
+rotulo_exibicao.pack()
+
+
 # ==================== Criar a tela ====================
 tela.mainloop()
