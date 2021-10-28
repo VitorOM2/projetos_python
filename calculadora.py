a339df4ca44053fd1de715320b6bb01ce3d275b7
VitorOM2 <viktormarque2053@gmail.com>
2021-10-27 23:18:09 -0300

    Tela criada

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+# ==================== Importações ====================
+from tkinter import *
+
+
+# ==================== Instanciação da Tela ====================
+tela = Tk()
+tela.geometry ( "500x500"   ) # Tamanho da tela
+tela.title    ("Calculadora") # Titulo da tela
+
+
+# ==================== Criar a tela ====================
+tela.mainloop()
