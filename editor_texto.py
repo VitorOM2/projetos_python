c70d5d58ce4dfab5529dd4a762e3193c90760eee
VitorOM2 <viktormarque2053@gmail.com>
2021-11-01 22:57:36 -0300

    Projeto criado

---
+++ editor_texto.py
@@ -0,0 +1,35 @@
+# ==================== Importações ====================
+import os
+from tkinter import *
+from tkinter import filedialog, colorchooser, font
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+
+
+# ==================== Funções ====================
+def mudar_cor():
+    pass
+
+def mudar_fonte(*args):
+    pass
+
+def novo_arquivo():
+    pass
+
+def salvar_arquivo():
+    pass
+
+def cortar():
+    pass
+
+def copiar():
+    pass
+
+def colar():
+    pass
+
+def sobre():
+    pass
+
+def sair():
+    pass
